259789d4f215361d1e01c1ce9eb6ea4cd5d4e235
Jeroen Roelant <s121087@ap.be>
2022-12-19 14:06:08 +0100

    fixed closet guy

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -10,7 +10,7 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        UnityEngine.XR.InputTracking.disablePositionalTracking = false;
     }
 
     // Update is called once per frame
