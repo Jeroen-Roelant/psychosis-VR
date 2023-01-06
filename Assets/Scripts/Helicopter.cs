5b4dfc4220ecced63a3a122fed88b7cc71775a76
Jeroen Roelant <s121087@ap.be>
2023-01-06 16:01:57 +0100

    Helikopter

---
+++ Assets/Scripts/Helicopter.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Helicopter : MonoBehaviour
+{
+    private Vector3 _startPosition;
+    private Vector3 _endPosition;
+    private bool _on;
+    
+    public Transform endPoint;
+    
+    public float speed;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _startPosition = this.gameObject.transform.position;
+        _endPosition = endPoint.position;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HelicopterFlight(_on);
+    }
+
+    void HelicopterFlight(bool on)
+    {
+        if(on)
+        {
+            var step =  speed * Time.deltaTime;
+            this.gameObject.transform.position = Vector3.MoveTowards(this.gameObject.transform.position, _endPosition, step);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        _on = true;
+        this.gameObject.GetComponent<AudioSource>().Play();
+        Debug.Log("HELIKOPTER HELIKOPTER");
+    }
+}
